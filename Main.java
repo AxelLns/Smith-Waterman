8496774943e210510e3b2094145703cd7d0c77c0
AxelLns <axel.loones@gmail.com>
2023-08-30 17:04:07 -0400

    File reading

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        String path = "./DNA_data.txt";
+        File file = new File(path);
+        try {
+            Scanner sc = new Scanner(file);
+            System.out.println("File found");
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
