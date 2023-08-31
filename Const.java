a9d7cb8c71d13f03414a331a928630e1258ee676
AxelLns <axel.loones@gmail.com>
2023-08-31 15:44:10 -0400

    Classes created

---
+++ Const.java
@@ -0,0 +1,5 @@
+public class Const {
+    static final String white = "\u001B[37m";
+    static final String[] colors = { "\u001B[32m", "\u001B[33m", "\u001B[34m", "\u001B[35m", "\u001B[36m",
+            "\u001B[31m" };
+}
