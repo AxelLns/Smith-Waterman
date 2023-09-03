402862a58934802595bf4a8658eb000fc0523cc3
Pulsar94 <94862548+Pulsar94@users.noreply.github.com>
2023-09-03 13:05:42 -0400

    documentation reader

--- Reader.java
+++ Reader.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class Reader {
     // Method to read DNA sequences from a file
     public static ArrayList<String> readFile() {
-        String path = "./DNA_data.txt";
+        String path = "./DNA_data.txt"; // Path of the document .txt who stock the DNA sequences
         File file = new File(path);
-        ArrayList<String> sequences = new ArrayList<>();
+        ArrayList<String> sequences = new ArrayList<>(); // Initialization of the object "sequences" to be a resizable array
 
-        try (Scanner sc = new Scanner(file)) {
+        try (Scanner sc = new Scanner(file)) { // Initialization of "sc" object who contain the whole DNA.txt read by scanner
             // Loop through each line in the file
-            while (sc.hasNextLine()) {
+            while (sc.hasNextLine()) { // Verify if "sc" contain a line of data
                 sequences.add(sc.nextLine()); // Add each line to the ArrayList
             }
         } catch (FileNotFoundException e) {
@@ -22,8 +22,8 @@ public class Reader {
 
         //display the sequences
         for (int i = 0; i < sequences.size(); i++) {
-            System.out.printf(Const.white +">random sequence %d consisting of %d bases.\n\t", i + 1, sequences.size());
-            System.out.println(Const.white +sequences.get(i));
+            System.out.printf(Const.white +">random sequence %d consisting of %d bases.\n\t", i + 1, sequences.size()); // Display sequence and base number
+            System.out.println(Const.white +sequences.get(i)); // display the DNA sequence
         }
 
         return sequences;
